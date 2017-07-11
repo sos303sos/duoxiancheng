abac16b5baf3f234739f06c45a1d7d2ed2be64c1
95129 <95129@DESKTOP-LP33PPV>
2017-07-11 21:16:51 +0800

    更新

--- src/chapter3/visibility/SynchronizedInteger.java
+++ src/chapter3/visibility/SynchronizedInteger.java
@@ -1,5 +1,20 @@
 package chapter3.visibility;
 
+/**
+ * 程序清单3-3 线程安全的可变整数类
+ * 
+ * @author 95129
+ *
+ */
 public class SynchronizedInteger {
+	private int value;
+
+	public synchronized int getValue() {
+		return value;
+	}
+
+	public synchronized void setValue(int value) {
+		this.value = value;
+	}
 
 }
